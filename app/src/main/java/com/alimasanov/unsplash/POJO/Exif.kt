139e47af6c2ed6a52a65af80266adcc83f1017bc
Alim <asanov.alim.i215@gmail.com>
2020-01-04 22:30:41 +0300

    created data class Exif

---
+++ app/src/main/java/com/alimasanov/unsplash/POJO/Exif.kt
@@ -0,0 +1,10 @@
+package com.alimasanov.unsplash.POJO
+
+data class Exif {
+    var make: String? = null
+    var model: String? = null
+    var exposure_time: String? = null
+    var aperture: String? = null
+    var focal_length: String? = null
+    var iso: Int? = null
+}
--- app/src/main/java/com/alimasanov/unsplash/POJO/Photo.kt
+++ app/src/main/java/com/alimasanov/unsplash/POJO/Photo.kt
@@ -1,7 +1,15 @@
 package com.alimasanov.unsplash.POJO
 
-class Photo() {
-    var id: String
-    var created_at: String
-    var
+data class Photo() {
+    var id: String? = null
+    var created_at: String? = null
+    var updated_at: String? = null
+    var width: Int? = null
+    var height: Int? = null
+    var color: String? = null
+    var downloads: Int? = null
+    var likes: Int? = null
+    var liked_by_user: Boolean? = null
+    var description: String? = null
+
 }
