ccc8a20018bacbc5af6dc3e9f23c7e96dc184af4
Alim <asanov.alim.i215@gmail.com>
2020-01-05 22:41:56 +0300

    исправление ошибок с бд

--- app/src/main/java/com/alimasanov/unsplash/DB/UnsplashDB.kt
+++ app/src/main/java/com/alimasanov/unsplash/DB/UnsplashDB.kt
@@ -13,12 +13,20 @@ class UnsplashDB(context: Context?): SQLiteOpenHelper(context,
 ) {
 
     override fun onCreate(db: SQLiteDatabase) {
-        val createUnsplashTable = "CREATE TABLE $TABLE_NAME (" +
+        val createUnsplashTable = "CREATE TABLE $TABLE_NAME (                    " +
                                     "   $COLUMN_ID              INTEGER PRIMARY KEY,    " +
                                     "   $COLUMN_PHOTO           TEXT,                   " +
                                     "   $COLUMN_DESCRIPTION     TEXT,                   " +
-                                    "   $COLUMN_LOCATION        TEXT)                   "
+                                    "   $COLUMN_LOCATION        TEXT,                   " +
+                                    "   $COLUMN_TIMESTAMP       TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
+
+        val insertIntoTable = " INSERT INTO $TABLE_NAME($COLUMN_ID, $COLUMN_PHOTO, $COLUMN_DESCRIPTION, $COLUMN_LOCATION)" +
+                " VALUES(1,                                                                         " +
+                "       'https://images.unsplash.com/photo-1417325384643-aac51acc9e5d?q=75&fm=jpg', " +
+                "       'A man drinking a coffee.',                                                 " +
+                "       'Montreal, Canada')                                                         "
         db.execSQL(createUnsplashTable)
+        db.execSQL(insertIntoTable)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
@@ -57,5 +65,6 @@ class UnsplashDB(context: Context?): SQLiteOpenHelper(context,
         const val COLUMN_PHOTO = "ID_Photo"
         const val COLUMN_DESCRIPTION = "Description"
         const val COLUMN_LOCATION = "Location"
+        const val COLUMN_TIMESTAMP = "timestamp"
     }
 }
