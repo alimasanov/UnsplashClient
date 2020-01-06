e96f75b4b862e47678d958e861da9191cc5e288f
Alim <asanov.alim.i215@gmail.com>
2020-01-06 19:57:54 +0300

    поправил cardItem

--- app/src/main/java/com/alimasanov/unsplash/adapters/UnsplashAdapter.kt
+++ app/src/main/java/com/alimasanov/unsplash/adapters/UnsplashAdapter.kt
@@ -1,10 +1,12 @@
 package com.alimasanov.unsplash.adapters
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.alimasanov.unsplash.R
@@ -22,25 +24,48 @@ class UnsplashAdapter(context: Context?, val photos: List<Photo>?):
         return photos!!.count()
     }
 
+    @SuppressLint("WrongConstant")
     override fun onBindViewHolder(holder: UnsplashViewHolder, position: Int) {
         val photo: Photo = photos!![position]
 
-        val cardImageLink: String? = photo.urls?.regular
-        val cardDesc: String? = photo.description
-        val cardLoc: String? = "${photo.location?.country}, ${photo.location?.city}"
-
+        val cardImageLink: String? = photo.urls?.small
         Picasso.get()
             .load(cardImageLink)
             .placeholder(R.drawable.ic_picasso_placeholder)
             .error(R.drawable.ic_picasso_error)
             .into(holder.card_image)
-        holder.card_desc.text = cardDesc
-        holder.card_location.text = cardLoc
+
+        val cardDesc: String? = photo.description
+        if (cardDesc == null){
+//            holder.card_desc.visibility = 0x00000004
+            holder.ll_main.removeView(cardDesc)
+        }
+        else holder.card_desc.text = cardDesc
+
+        val cardLoc: String? = "${photo.location?.country}, ${photo.location?.city}"
+        if (photo.location?.country !== null && photo.location?.city !== null){
+            val cardLoc: String? = "${photo.location?.country}, ${photo.location?.city}"
+            holder.card_location.text = cardLoc
+        } else if(photo.location?.country == null && photo.location?.city !== null){
+            val cardLoc: String? = photo.location?.city
+            holder.card_location.text = cardLoc
+        } else if(photo.location?.country !== null && photo.location?.city == null){
+            val cardLoc: String? = photo.location?.country
+            holder.card_location.text = cardLoc
+        } else {
+//            holder.card_location.visibility = 0x00000004
+//            holder.card_image_location.visibility = 0x00000004
+            holder.ll.removeAllViews()
+        }
+
     }
 
     class UnsplashViewHolder(itemView: View,
                              val card_image: ImageView = itemView.findViewById(R.id.card_image),
                              val card_desc: TextView = itemView.findViewById(R.id.card_desc),
-                             val card_location: TextView = itemView.findViewById(R.id.card_location)):
+                             val card_location: TextView = itemView.findViewById(R.id.card_location),
+                             val card_image_location: ImageView = itemView.findViewById(R.id.image_location),
+                             val ll_main: LinearLayout = itemView.findViewById(R.id.ll_main),
+                             val ll: LinearLayout = itemView.findViewById(R.id.ll)):
         RecyclerView.ViewHolder(itemView)
 }
